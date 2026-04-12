9ab839329755d92947574b1944ef8e4e082dc82d
Tony Jugo <caohoangnhat58@gmail.com>
2026-04-12 21:47:15 +0700

    ignore sth in conversation

--- DreamGuard.BE.DAL/Models/Conversation.cs
+++ DreamGuard.BE.DAL/Models/Conversation.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace DreamGuard.BE.DAL.Models
@@ -13,9 +14,13 @@ namespace DreamGuard.BE.DAL.Models
         public Guid CustomerId { get; set; }
         public Guid StaffId { get; set; }
         public DateTime CreatedAt { get; set; }
+        [JsonIgnore]
         public TradeInOrder TradeInOrder { get; set; }
+        [JsonIgnore]
         public Customer Customer { get; set; }
+        [JsonIgnore]
         public Staff Staff { get; set; }
+        [JsonIgnore]
         public ICollection<ChatMessage> ChatMessages { get; set; }
     }
 }
